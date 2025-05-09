2ee63e7d338ee47d0f5082ef47c0b320acc7e621
GustaKowai <gusta.kowai@gmail.com>
2025-05-08 21:15:39 -0300

    mudança da cena bullet para a pasta certa.

--- weapons/revolver.gd
+++ weapons/revolver.gd
@@ -15,7 +15,7 @@ func _on_area_2d_body_entered(body):
 	if body.is_in_group("Jogador"):
 		var player = body
 		#Se o jogador pegar, define como a arma carregada e define a munição:
-		player.bullet_path = preload("res://Player/bullet.tscn")
+		player.bullet_path = preload("res://weapons/bullet.tscn")
 		player.weapon_path = preload("res://weapons/revolver_sprite.png")
 		player.equiped_weapon.texture = player.weapon_path
 		player.weapon_cooldown = 0.2
