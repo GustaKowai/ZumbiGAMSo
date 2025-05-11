5e9a3375afe2daf911cc82b05cf539182fdc3d3b
Leandro Colaes <colaesleandro@gmail.com>
2025-05-11 19:46:18 -0300

    Gunshot - LC - Tarefa das Armas R1 - Colando o arquivo correto
    Obj: colocar o path correto do sprite da arma.
    A Fazer: alterar alcance, dano e tamanho do drop.
    Erros: a arma não está dropando mesmo alterando a lista de drop e as chances

--- weapons/revolver.gd
+++ weapons/revolver.gd
@@ -16,7 +16,7 @@ func _on_area_2d_body_entered(body):
 		var player = body
 		#Se o jogador pegar, define como a arma carregada e define a munição:
 		player.bullet_path = preload("res://weapons/bullet.tscn")
-		player.weapon_path = preload("res://weapons/gunshot.png")
+		player.weapon_path = preload("res://weapons/revolver_sprite.png")
 		player.equiped_weapon.texture = player.weapon_path
 		player.weapon_cooldown = 0.2
 		player.ammo = 6
