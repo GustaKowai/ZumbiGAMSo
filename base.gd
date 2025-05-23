c0b1fa5af9eb37ea1ef70b54bc6cf78443ae2db1
GustaKowai <gusta.kowai@gmail.com>
2025-05-23 17:46:18 -0300

    Testes com mudanças no tilemap durante execucao

--- base.gd
+++ base.gd
@@ -1,7 +1,7 @@
 extends TileMapLayer
 
 @onready var obstacles:TileMapLayer = $"../Obstacles Collision"
-
+@onready var timer:Timer = $Timer
 func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
 	if coords in obstacles.get_used_cells_by_id(0):
 		return true
@@ -9,3 +9,24 @@ func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
 
 func _tile_data_runtime_update(coords: Vector2i, tile_data: TileData) -> void:
 	tile_data.set_navigation_polygon(0,null)
+
+
+#Função de teste, vai transformando a rua próxima do jogador em água. A ideia é usar isso para criar obstáculos para o jogador e evitar que ele fique muito tempo parado no mesmo lugar
+#func _on_timer_timeout() -> void:
+	#if GameManager.player.position:
+		#var player_position = GameManager.player.position
+		#var rand_vector = Vector2(randf_range(90,800),0)
+		#rand_vector = rand_vector.rotated(randf_range(0,2*PI))
+		#var position_choosen = player_position + rand_vector
+		#print(player_position,position_choosen)
+		#var tile_position = local_to_map(position_choosen)
+		#print(tile_position)
+		#var tile_data:TileData = get_cell_tile_data(tile_position)
+		#print(tile_data)
+		#if tile_data:
+			#var e_rua = tile_data.get_custom_data("e_rua")
+			#if e_rua:
+				#set_cell(tile_position,0,Vector2i(2,5))
+			#else:
+				#print("não é rua")
+		#timer.start()
--- treinando_coisas.gd
+++ treinando_coisas.gd
@@ -1,3 +1,39 @@
 extends Node2D
 
 @onready var tilemap:TileMapLayer = $TileMapLayer
+@onready var timer:Timer = $TileMapLayer/Timer
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("attack"):
+		var local_position = tilemap.local_to_map(GameManager.player.position)
+		var tile_data:TileData = tilemap.get_cell_tile_data(local_position)
+		if tile_data:
+			var is_green = tile_data.get_custom_data("green")
+			if is_green:
+				#tilemap.set_cell(local_position + Vector2i(1,0),0,Vector2i(0,3))
+				#tilemap.set_cell(local_position + Vector2i(-1,0),0,Vector2i(0,3))
+				#tilemap.set_cell(local_position + Vector2i(-1,-1),0,Vector2i(0,3))
+				#tilemap.set_cell(local_position + Vector2i(-1,1),0,Vector2i(0,3))
+				#tilemap.set_cell(local_position + Vector2i(1,1),0,Vector2i(0,3))
+				#tilemap.set_cell(local_position + Vector2i(1,-1),0,Vector2i(0,3))
+				#tilemap.set_cell(local_position + Vector2i(0,-1),0,Vector2i(0,3))
+				#tilemap.set_cell(local_position + Vector2i(0,1),0,Vector2i(0,3))
+				tilemap.set_cell(local_position,0,Vector2i(0,3))
+				#TOMAR CUIDADO COM ISSO, PODE PRENDER O JOGADOR
+func _on_timer_timeout() -> void:
+	if GameManager.player.position:
+		var player_position = GameManager.player.position
+		var rand_vector = Vector2(randf_range(30,100),0)
+		rand_vector = rand_vector.rotated(randf_range(0,2*PI))
+		var position_choosen = player_position + rand_vector
+		print(player_position,position_choosen)
+		var tile_position = tilemap.local_to_map(position_choosen)
+		print(tile_position)
+		var tile_data:TileData = tilemap.get_cell_tile_data(tile_position)
+		print(tile_data)
+		if tile_data:
+			var is_green = tile_data.get_custom_data("green")
+			if is_green:
+				tilemap.set_cell(tile_position,0,Vector2i(0,3))
+			else:
+				print("não é rua")
+		timer.start()
