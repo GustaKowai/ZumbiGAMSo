178f8a3fbfc446004e16c7f5a3586babee48aacb
LuisCury <luis.cury@estudante.ufscar.br>
2025-06-01 23:27:08 -0300

    Add poçao de cura - ainda sem a barra de vida (O anterior n add nd. Commit errado :/)

---
+++ items/Itens_cura/cura_pequena.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+@export var item:PackedScene
+
+func _on_area_2d_body_entered(body):
+	print("pegou")
+	if body.is_in_group("Jogador"): #Checa se o corpo que entrou no range é o player
+		GameManager.item_collected.emit("res://items/Itens_cura/potion__pequeno_pokemon.png") #Emite um sinal avisando que uma arma foi coletada
+		var player = body
+		var item_instance = item.instantiate()
+		item_instance.item_loaded = load("res://items/Itens_cura/cura_pequena_objeto.tscn")
+		player.add_child(item_instance)
+		queue_free()
---
+++ items/Itens_cura/cura_pequena_objeto.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var quantidade_cura: int
+
+func _ready() -> void:
+	#Chamada para a funcao que ira curar
+	heal_player(quantidade_cura)
+	
+func heal_player(vida_curada: int) -> void:
+	#variavael que armazena a quantidade de vida do player antes da cura
+	var controle_de_vida = GameManager.player.player_health
+	#Casos em que com a cura a vida do player passaria do limite
+	#Ex: se o jogador tiver 37 de vida ao curar 5 de vida ele deve ir para 40(limte de vida) e nao 42
+	if controle_de_vida > (GameManager.player.max_health - vida_curada) and controle_de_vida <= GameManager.player.max_health:
+		GameManager.player.player_health = GameManager.player.max_health
+		print("curou para vida maxima")
+	#Caso geral em que so adiciona a quantidade da pocao a vida do player
+	else:
+		GameManager.player.player_health += vida_curada
+		print("curou 5")
+	queue_free()
