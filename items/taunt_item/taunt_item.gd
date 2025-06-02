f264906f6a83421e7c000f02ffcf9f8a5352c72b
GustaKowai <gusta.kowai@gmail.com>
2025-06-02 17:09:16 -0300

    Adicionado o sprite do icon na UI

--- items/taunt_item/taunt_item.gd
+++ items/taunt_item/taunt_item.gd
@@ -2,9 +2,9 @@ extends Sprite2D
 @export var item:PackedScene
 
 func _on_area_2d_body_entered(body):
-	print("pegou")
+	#print("pegou")
 	if body.is_in_group("Jogador"): #Checa se o corpo que entrou no range é o player
-		GameManager.item_collected.emit("res://a-radio-tape-in-pixel-art-style-vector.jpg") #Emite um sinal avisando que uma arma foi coletada
+		GameManager.item_collected.emit("res://items/taunt_item/item_atrai_icon.png") #Emite um sinal avisando que uma arma foi coletada
 		var player = body
 		var item_instance = item.instantiate()
 		item_instance.item_loaded = load("res://items/taunt_item/taunt_object.tscn")
