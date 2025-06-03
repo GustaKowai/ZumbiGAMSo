580d128272588214aa494d4d509a6cb71a9c0e18
GustaKowai <gusta.kowai@gmail.com>
2025-06-03 16:31:01 -0300

    Turret criada. Você pode mudar a munição dela facilmente só trocando o bullet_scene carregado. Da inclusive para colocar para ser dinâmico isso (Mudando de forma aleatória ou com upgrades)

---
+++ items/turret/turret_item.gd
@@ -0,0 +1,12 @@
+extends Node2D
+@export var item:PackedScene
+
+func _on_area_2d_body_entered(body):
+	#print("pegou")
+	if body.is_in_group("Jogador"): #Checa se o corpo que entrou no range é o player
+		GameManager.item_collected.emit("res://items/turret/turret_item.png") #Emite um sinal avisando que uma arma foi coletada
+		var player = body
+		var item_instance = item.instantiate()
+		item_instance.item_loaded = load("res://items/turret/turret_object.tscn")
+		player.add_child(item_instance)
+		queue_free()
---
+++ items/turret/turret_object.gd
@@ -0,0 +1,43 @@
+extends StaticBody2D
+
+@onready var range_area:Area2D = $Range
+@onready var marker:Marker2D = $Marker2D
+@export var turret_duration:float
+@export var bullet_scene:PackedScene
+var enemy_position:Vector2
+var shooting_cooldown = 1
+var shooting:bool
+
+func _ready() -> void:
+	shooting = false
+	
+func _process(delta: float) -> void:
+	turret_duration -=delta
+	shooting_cooldown -= delta
+	if shooting_cooldown <= 0:
+		shooting = false
+		shooting_cooldown = 1
+	try_shoot()
+	if turret_duration <=0:
+		despawn()
+
+func fire_bullet(target):
+	shooting = true
+	#Determina a direção do tiro e cria a bala
+	var bullet = bullet_scene.instantiate()
+	bullet.dir = Vector2.RIGHT.angle_to(target-global_position)
+	print(bullet.dir)
+	bullet.pos = marker.global_position
+	bullet.rota = Vector2.RIGHT.angle_to(target-global_position)
+	get_parent().add_child(bullet)#Instancia a bala
+	
+func despawn():
+	queue_free()
+
+func try_shoot():
+	if shooting:return
+	var areas:Array[Area2D] = range_area.get_overlapping_areas() #Detecta todas as áreas que estão dentro da área de dano do inimigo
+	for area in areas:
+		if area.is_in_group("EnemyHitBox"): #Detecta se alguma área detectada é um inimigo
+			if shooting:return
+			fire_bullet(area.global_position)
