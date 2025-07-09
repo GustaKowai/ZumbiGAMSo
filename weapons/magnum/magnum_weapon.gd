2c0b9f58f3ac177913c5055232e5380c2b35ea65
Leandro Colaes <colaesleandro@gmail.com>
2025-07-09 16:52:58 -0300

    Upgrade Magnum-Ricochete - Progresso
    Aplicandos os passos iniciais

--- weapons/magnum/magnum_weapon.gd
+++ weapons/magnum/magnum_weapon.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var bullet_path = preload("res://weapons/magnum/bullet_magnum.tscn")
+@onready var bullet_path = preload("res://weapons/magnum/bullet_magnum_ricochetear.tscn")
 @onready var player = get_parent()
 @onready var animation_player = $AnimationPlayer
 @onready var marker = $Marker2D
