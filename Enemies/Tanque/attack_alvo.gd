7cca201c54e3b5c0f41758b309e618d2ce11cb27
GustaKowai <gusta.kowai@gmail.com>
2025-07-13 21:10:23 -0300

    Correcoes pequenas

--- Enemies/Tanque/attack_alvo.gd
+++ Enemies/Tanque/attack_alvo.gd
@@ -15,6 +15,7 @@ var attack_cooldown:float
 var shoot_direction:String
 @export var alvo_scene:PackedScene
 var ID_tiro:int = 0
+var modo_metralhadora:float = false
 
 func _ready():
 	enemy = get_parent()
@@ -43,23 +44,25 @@ func try_shoot():
 			player_position = GameManager.taunt_position
 		var point_weapon:Vector2 = player_position - enemy.global_position
 		if point_weapon.length_squared() >= range*1000: return
-		if point_weapon.length_squared() <= 100000: return
-		if point_weapon.x <= accuracy/4 and point_weapon.x >= -accuracy/4:
-			if point_weapon.y < 0:
-				shoot_direction = "Up"
-				shoot()
-			else:
-				shoot_direction = "Down"
-				shoot()
-		if point_weapon.y <=accuracy and point_weapon.y >= 0:
-			#print_debug(point_weapon.y)
-			if point_weapon.x > 0:
-				shoot_direction = "Right"
-				shoot()
-			else:
-				shoot_direction = "Left"
-				shoot()
-			
+		if modo_metralhadora:
+			pass
+		else:
+			if point_weapon.x <= accuracy/4 and point_weapon.x >= -accuracy/4:
+				if point_weapon.y < 0:
+					shoot_direction = "Up"
+					shoot()
+				else:
+					shoot_direction = "Down"
+					shoot()
+			if point_weapon.y <=accuracy and point_weapon.y >= 0:
+				#print_debug(point_weapon.y)
+				if point_weapon.x > 0:
+					shoot_direction = "Right"
+					shoot()
+				else:
+					shoot_direction = "Left"
+					shoot()
+				
 func shoot():
 	if enemy.is_attacking:return
 	enemy.is_attacking = true
--- Enemies/Zumbi_normal_1/zombie_1.gd
+++ Enemies/Zumbi_normal_1/zombie_1.gd
@@ -71,12 +71,13 @@ func die():
 	await drop_item()
 	await drop_coins()
 	GameManager.kills_count += 1
-	if alma_dropada == 1:
+	if alma_dropada == 0:
 		GameManager.alma_comum += 1
-	if alma_dropada == 2:
+	if alma_dropada == 1:
 		GameManager.alma_incomum += 1
-	if alma_dropada == 3:
+	if alma_dropada == 2:
 		GameManager.alma_rara += 1
+	print_debug(alma_dropada)
 	GameManager.zombie_died.emit()
 	GameManager.infection_level -= GameManager.infection_power
 	queue_free()
