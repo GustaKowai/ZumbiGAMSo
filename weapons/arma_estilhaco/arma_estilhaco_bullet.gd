901bb795b6b19368a66e53e5c5b57716eaf3d707
GustaKowai <gusta.kowai@gmail.com>
2025-07-22 18:02:47 -0300

    Refatoração da bala da arma de estilhaços

--- weapons/arma_estilhaco/arma_estilhaco_bullet.gd
+++ weapons/arma_estilhaco/arma_estilhaco_bullet.gd
@@ -1,43 +1,21 @@
-extends CharacterBody2D
+extends Bullet_base
 
-var pos:Vector2
-var rota:float
-var dir: float
-var speed:int = 650
-var bullet_damage:int = 16
-var piercing:int = 0
 var numero_estilhacos:int = 4
 @export var estilhacinho:PackedScene
-@export var bullet_hit_scene:PackedScene
 
 func _ready():
-	global_position = pos
-	global_rotation = rota
+	set_start_position()
 	bullet_damage += GameManager.upgrade_estilhaco[1]
 	numero_estilhacos += GameManager.upgrade_estilhaco[2]
 func _physics_process(delta):
-	velocity = Vector2(speed,0).rotated(dir)
-	move_and_slide()
-	if GameManager.player:
-		if position.distance_squared_to(GameManager.player.position) > 1000000:
-			queue_free()
-	else:
-		queue_free()
+	move_front()
+	desapear_on_distance()
 
 func _on_bullet_hit_box_area_entered(area):
-	if area.is_in_group("EnemyHitBox"):
-		var enemy:Enemy  = area.get_parent()
-		enemy.damage(bullet_damage)
-		set_bullet_hit()
-		var enemy_position = enemy.global_position
+	if area.is_in_group("EnemyHitBox") or area.is_in_group("construcao"):
 		dividir()
-		if piercing <= 0:
-			queue_free()
-		piercing -= 1
-	if area.is_in_group("construcao"):
-		set_bullet_hit()
-		dividir()
-		queue_free()
+		hit_enemy(area)
+		desapear_on_hit_building(area)
 
 func dividir():
 	var angle_offset = PI/numero_estilhacos
@@ -49,10 +27,3 @@ func dividir():
 		sub.rotation = rotation + direction*2*PI/numero_estilhacos + angle_offset
 	
 	queue_free()
-	
-func set_bullet_hit():
-	if bullet_hit_scene:
-		var bullet_hit = bullet_hit_scene.instantiate()
-		bullet_hit.global_position = global_position
-		bullet_hit.global_rotation = global_rotation
-		get_parent().get_parent().add_child(bullet_hit)
--- weapons/arma_estilhaco/estilhacinho.gd
+++ weapons/arma_estilhaco/estilhacinho.gd
@@ -1,26 +1,14 @@
-extends CharacterBody2D
+extends Bullet_base
 
-var pos:Vector2
-var rota:float
-var dir: float
-var speed:int = 800
-var piercing = 0
-var bullet_damage
 var tempo_sem_dar_dano = 0.05
-@export var bullet_hit_scene:PackedScene
 
 func _ready():
-	global_position = pos
-	global_rotation = rota
+	set_start_position()
 	
 func _physics_process(delta):
 	velocity = Vector2.RIGHT.rotated(rotation) * speed
 	move_and_slide()
-	if GameManager.player:
-		if position.distance_squared_to(GameManager.player.position) > 1000000:
-			queue_free()
-	else:
-		queue_free()
+	desapear_on_distance()
 
 func _process(delta: float) -> void:
 	tempo_sem_dar_dano -= delta
@@ -28,22 +16,5 @@ func _process(delta: float) -> void:
 func _on_bullet_hit_box_area_entered(area):
 	if tempo_sem_dar_dano > 0:
 		return
-	
-	if area.is_in_group("EnemyHitBox"):
-		var enemy:Enemy  = area.get_parent()
-		enemy.damage(bullet_damage)
-		set_bullet_hit()
-		if piercing <= 0:
-			queue_free()
-		piercing -= 1
-	if area.is_in_group("construcao"):
-		set_bullet_hit()
-		#print("Acertei um predio")
-		queue_free()
-		
-func set_bullet_hit():
-	if bullet_hit_scene:
-		var bullet_hit = bullet_hit_scene.instantiate()
-		bullet_hit.global_position = global_position
-		bullet_hit.global_rotation = global_rotation
-		get_parent().get_parent().add_child(bullet_hit)
+	hit_enemy(area)
+	desapear_on_hit_building(area)
