a110559ab8d139ef7fd24ad00c99887d691c0962
GustaKowai <gusta.kowai@gmail.com>
2025-07-22 18:38:03 -0300

    Voltando as armas sem upgrade para o reset

--- game_manager.gd
+++ game_manager.gd
@@ -92,9 +92,9 @@ func reset_game_status():
 	GameManager.stamina_max_up = 0
 	GameManager.stamina_rege_up = 0
 	GameManager.sword_damage_up = 0
-	GameManager.upgrade_revolver = [0,0,0,1] #[munição,dano,perfuração,bala curva]
+	GameManager.upgrade_revolver = [0,0,0,0] #[munição,dano,perfuração,bala curva]
 	GameManager.upgrade_metralhadora = [0,0,100,100,0] #[munição,dano,spreed,velocidade de tiro,segue alvo]
-	GameManager.upgrade_shotgun = [0,0,100,100,0,1] #[munição,dano,spread,alcance,quandidade de estilhaços,upgrade]
+	GameManager.upgrade_shotgun = [0,0,100,100,0,0] #[munição,dano,spread,alcance,quandidade de estilhaços,upgrade]
 	GameManager.upgrade_magnum = [0,0] #[munição,dano]
 	GameManager.upgrade_bazuca = [0,0,0] #[munição,dano,Upgrade da bala dividir]
 	GameManager.upgrade_estilhaco = [0,0,0] #[munição,dano,mais estilhaços]
