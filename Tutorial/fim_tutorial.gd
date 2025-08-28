2dd4af51f2c8adf356d1d2147eb4a5fb517e5ab4
GustaKowai <gusta.kowai@gmail.com>
2025-08-27 21:37:52 -0300

    Tutorial linkado no jogo

--- Tutorial/fim_tutorial.gd
+++ Tutorial/fim_tutorial.gd
@@ -3,5 +3,6 @@ extends CanvasLayer
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			
+			GameManager.scene_to_load = "res://UI/menu.tscn"
+			get_tree().change_scene_to_file("res://UI/loading.tscn")
 			queue_free()
