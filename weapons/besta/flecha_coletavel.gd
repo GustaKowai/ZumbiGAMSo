a7f9c737cf2e4829135fdacb29b76a73de17663b
GustaKowai <gusta.kowai@gmail.com>
2025-10-30 13:00:48 -0300

    mudança na flecha coletavel

--- weapons/besta/flecha_coletavel.gd
+++ weapons/besta/flecha_coletavel.gd
@@ -1,15 +1,7 @@
 extends Arma_Coletavel
 
-func _ready() -> void:
-	#print_debug("dropei uma flecha no chao")
-	pass
 func _on_area_2d_body_entered(body):
 	coleta_arma(body)
 
 func _on_timer_timeout() -> void:
 	queue_free()
-
-
-func _on_tree_entered() -> void:
-	#print_debug("entrei")
-	pass
